cf02cdc2d6e638424dd06a80966ed01b7cd682df
Tiền mỹ hiệu <tienmyhieu@gmail.com>
2024-10-13 08:47:59 -0400

    initialize

---
+++ src/lib/Html/TmhHtmlDocumentFactory.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace lib\Html;
+
+use lib\HtmlComponent\TmhHtmlComponentFactory;
+
+class TmhHtmlDocumentFactory
+{
+    private TmhHtmlElementFactory $elementFactory;
+    private TmhHtmlComponentFactory $htmlComponentFactory;
+    private TmhHtmlNodeTransformer $nodeTransformer;
+
+    public function __construct(
+        TmhHtmlElementFactory   $elementFactory,
+        TmhHtmlComponentFactory $htmlComponentFactory,
+        TmhHtmlNodeTransformer $nodeTransformer
+    ) {
+        $this->elementFactory = $elementFactory;
+        $this->htmlComponentFactory = $htmlComponentFactory;
+        $this->nodeTransformer = $nodeTransformer;
+    }
+
+    public function create(array $entity): string
+    {
+        $nodes = $this->elementFactory->html([$this->head($entity), $this->body($entity)], $entity['lang']);
+        return $this->nodeTransformer->toHtml($nodes);
+    }
+
+    private function body(array $entity): array
+    {
+        $marginLeft = $this->elementFactory->marginLeft();
+        $marginRight = $this->elementFactory->marginRight();
+        $childNodes = [];
+        foreach ($entity['attributes'] as $attributeGroup) {
+            $componentLists = [];
+            foreach ($attributeGroup as $attributeList) {
+                $components = [];
+                foreach ($attributeList as $attribute) {
+                    $htmlComponent = $this->htmlComponentFactory->create($attribute['component_type']);
+                    $components = array_merge($components, $htmlComponent->get($attribute));
+                }
+                $componentLists[] = $this->elementFactory->componentList($components);
+            }
+            $childNodes[] = $this->elementFactory->componentGroup($componentLists);
+        }
+        $center = $this->elementFactory->center($childNodes);
+        $body = $this->elementFactory->contentBody([$marginLeft, $center, $marginRight]);
+        return $this->elementFactory->body([$body]);
+    }
+
+    private function head(array $entity): array
+    {
+        return $this->elementFactory->head($entity['description'], $entity['keywords'], $entity['documentTitle']);
+    }
+}
